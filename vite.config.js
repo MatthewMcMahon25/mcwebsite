1001ef22b35706b6bc44d4674a68f5608d9ed923
Matthew  McMahon <macmatt5@gmail.com>
2025-07-09 17:48:18 -0400

    Move newsletter to public/ and prep for static site deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
+  root: 'public',
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'), 
-        newsletter: resolve(__dirname, 'newsletter/index.html'),
-        post: resolve(__dirname, 'newsletter/post.html'),
-        subscribe: resolve(__dirname, 'newsletter/subscribe.html'),
-        book: resolve(__dirname, 'book/index.html'),
-      },
-    },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
-  publicDir: 'public', // 👈 Tells Vite to copy everything from /newsletter/ directly into dist/
 });
